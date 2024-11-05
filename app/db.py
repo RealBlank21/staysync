4248a1dace92c3e939a31a01ddec866b72424fe5
RealBlank21 <realblank21@gmail.com>
2024-11-06 05:36:56 +0800

    db

--- app/db.py
+++ app/db.py
@@ -1,8 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 
-mysql_url = "${{ MySQL.MYSQL_URL }}"
-print(mysql_url)
+mysql_url = "mysql://root:EAnDoELmcbiofXxjkVmUZoOsklrbBIno@junction.proxy.rlwy.net:30982/railway"
 
 try:
     db_config = mysql_url.split("://")[1].split("@")
